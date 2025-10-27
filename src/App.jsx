a1fca7b10736bf87fe94ed50a443ba2f2fbc059c
Alejandro Burrone @ ieion Corporation <alejandro.burrone@ieion.com>
2025-10-27 07:36:22 -0400

    feat: dynamic page title + full i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 // src/App.jsx
+import { useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import SearchBar from './components/SearchBar'
 
 function App() {
+  const { t, i18n } = useTranslation()
+
+  // Cambia dinámicamente el <title> de la pestaña según el idioma
+  useEffect(() => {
+    document.title = t('appName')
+  }, [i18n.language, t])
+
   return <SearchBar />
 }
 
